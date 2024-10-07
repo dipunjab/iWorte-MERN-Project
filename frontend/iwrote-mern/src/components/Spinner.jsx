65f86fb2657628c68c43e6592334ace94fc60b88
Usman <ghaniusman0287@gmail.com>
2024-10-07 22:42:15 +0500

    fixed css

--- frontend/iwrote-mern/src/components/Spinner.jsx
+++ frontend/iwrote-mern/src/components/Spinner.jsx
@@ -3,16 +3,7 @@ import spinner from "../assets/spinner.gif"
 
 function Spinner() {
   return (
-    <div style={{
-        width: "100vw",
-        position: "fixed",
-        top:"0",
-        height: "100vw",
-        backgroundColor: "#dad7cd",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-    }}>
+    <div className='position-fixed top-50 start-50'>
       <img src={spinner} alt="" style={{width: "100px"}}/>
     </div>
   )
