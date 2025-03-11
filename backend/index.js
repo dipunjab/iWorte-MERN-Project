e0ec26179b1f53b3f221436a20b6918e6d9d8c9e
Usman <ghaniusman0287@gmail.com>
2025-03-11 23:12:28 +0500

    fixing CORS

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express()
 //CORS
 app.use(cors(
     {
-        origin: "http://localhost:5173", // Adjust this to your frontend URL
+        origin: ["http://localhost:5173", "https://i-worte-mernbackend-api.vercel.app"],
         credentials: true // If using cookies or authentication headers
     }
 ))
